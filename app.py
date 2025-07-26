2f310a1babbbec78a2f61410c7c88568a24680b2
ahmadfathan <32026493+ahmadfathan@users.noreply.github.com>
2025-07-26 20:44:30 +0700

    feat: integrate browser automation

--- app.py
+++ app.py
@@ -9,7 +9,51 @@ from PyQt5.QtCore import pyqtSignal, QThread
 
 # Import your automator from your project
 from src.android.android_automator_v2 import AndroidAutomator, get_my_default_ui_automator2_options
+from src.browser.browser_automator_v2 import BrowserAutomator, get_my_default_chrome_options, get_user_ids
 
+from multiprocessing import Process, Event, set_start_method
+
+from time import sleep
+from src.adb_helpers import run_adb_command, get_connected_devices
+
+set_start_method("fork")
+
+def adb_push_file_to_android(filename, dest):
+    run_adb_command(f"push {filename} {dest}")
+
+def adb_trigger_scan_file(path):
+    run_adb_command(f"shell am broadcast -a android.intent.action.MEDIA_SCANNER_SCAN_FILE -d file://{path}")
+
+def download_QRIS(browser_automator, continue_event, qris_downloaded_event):
+    browser_automator.loop_downloads(continue_event, qris_downloaded_event)
+
+def scan_QRIS(android_automator, qris_downloaded_event, continue_event):
+    while True:
+        print("[SCAN QRIS] waiting QRIS downloaded..")
+        qris_downloaded_event.wait()
+        print("[SCAN QRIS] QRIS downloaded")
+        
+        # push the file to android device
+        # dest_folder = "/storage/emulated/0/Pictures"
+        # filename = get_newest_file_by_name("screenshots")
+
+        # adb_push_file_to_android(f"screenshots/{filename}", dest_folder)
+
+        # sleep(1)
+        # adb_trigger_scan_file(f"{dest_folder}/{filename}")
+        
+        # sleep(0.5)
+
+        # scan QRIS
+        android_automator.pay_qris_transaction()
+
+        print("Done!")
+
+        # emit continue event
+        continue_event.set()
+        
+        # clear events
+        qris_downloaded_event.clear()
 
 class PaymentWorker(QThread):
     payment_finished = pyqtSignal()
@@ -26,16 +70,47 @@ class PaymentWorker(QThread):
         device_udid = os.getenv("DEVICE_UDID")
         neo_pin = os.getenv("NEO_PIN")
 
+        base_url = os.getenv("WEB_BASE_URL")
+        username = os.getenv("WEB_CRED_USERNAME")
+        password = os.getenv("WEB_CRED_PASSWORD")
+
+        USER_IDS_PATH = "user_ids_5.txt"
+        user_ids = get_user_ids(USER_IDS_PATH)
+
+        continue_event = Event()
+        qris_downloaded_event = Event()
+
+        browser_automator = BrowserAutomator(base_url, get_my_default_chrome_options())
+        browser_automator.set_credentials(username, password)
+        browser_automator.set_user_ids(user_ids)
+
+        print("[BROWSER][INFO] Setting up..")
+        browser_automator.setup()
+        print("[BROWSER][INFO] Done.")
+
+        print("[INFO] Generating QRIS..")
+        browser_automator.generate_QRIS()
+        print("[INFO] Done.")
+
         options = get_my_default_ui_automator2_options(device_udid)
-        automator = AndroidAutomator(appium_server_url, options)
-        automator.set_credentials(neo_pin)
+        android_automator = AndroidAutomator(appium_server_url, options)
+        android_automator.set_credentials(neo_pin)
 
-        print("[INFO] Pay 5 QRIS transaction")
-        for i in range(5):
-            automator.pay_qris_transaction()
+        p_download_QRIS = Process(target=download_QRIS, args=(browser_automator, continue_event, qris_downloaded_event,))
+        p_scan_QRIS = Process(target=scan_QRIS, args=(android_automator, qris_downloaded_event, continue_event,))
+        
+        p_download_QRIS.start()
+        p_scan_QRIS.start()
 
-        print("Done!")
-        automator.quit()
+        sleep(3)
+
+        continue_event.set()
+
+        p_download_QRIS.join()
+        p_scan_QRIS.terminate()
+
+        browser_automator.quit()
+        android_automator.quit()
 
         # Emit signal after finishing
         self.payment_finished.emit()
@@ -137,9 +212,11 @@ class QRISAutoPayApp(QWidget):
             self.toggle_button.setEnabled(False)
 
     def get_device_udid(self):
-        """Simulate getting the UDID of the Android device."""
-        return "1234567890abcdef"  # Simulated. Replace with actual detection if needed.
-
+        devices = get_connected_devices()
+        if len(devices) == 0:
+            return ""
+        
+        return devices[0]
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
--- src/adb_helpers.py
+++ src/adb_helpers.py
@@ -2,6 +2,34 @@ import subprocess
 
 adb_path = "/Users/haus-dev/Library/Android/sdk/platform-tools/adb"
 
+def get_connected_devices():
+    try:
+        result = subprocess.run(
+            [adb_path, "devices"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
+        if result.returncode != 0:
+            print("ADB Error:\n", result.stderr)
+            return []
+
+        lines = result.stdout.strip().splitlines()
+        # Skip the first line: "List of devices attached"
+        device_lines = lines[1:]
+        devices = []
+
+        for line in device_lines:
+            parts = line.strip().split()
+            if len(parts) == 2 and parts[1] == "device":
+                devices.append(parts[0])
+
+        return devices
+
+    except FileNotFoundError:
+        print("ADB not found. Make sure it's installed and in your PATH.")
+        return []
+    
 def run_adb_command(command):
     try:
         result = subprocess.run(
