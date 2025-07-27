0025bf0914bd12451c147f1cdab1a37ac3d82a62
Ahmad Fathan <ahmadfathan1021@gmail.com>
2025-07-27 12:29:16 +0700

    feat: parameterize headless mode

--- src/browser/browser_automator_v2.py
+++ src/browser/browser_automator_v2.py
@@ -20,7 +20,7 @@ def get_user_ids(filename):
             ids.append(line.strip())
     return ids
 
-def get_my_default_chrome_options():
+def get_my_default_chrome_options(headless=True):
     # Create a dictionary for Chrome preferences
     chrome_prefs = {
         "credentials_enable_service": False,
@@ -32,9 +32,10 @@ def get_my_default_chrome_options():
     chrome_options = Options()
     chrome_options.add_experimental_option("prefs", chrome_prefs)
 
-    # chrome_options.add_argument("--headless")  # Runs Chrome in headless mode
-    # chrome_options.add_argument("--no-sandbox")
-    # chrome_options.add_argument("--disable-dev-shm-usage")
+    if headless:
+        chrome_options.add_argument("--headless")  # Runs Chrome in headless mode
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
 
     return chrome_options
 
